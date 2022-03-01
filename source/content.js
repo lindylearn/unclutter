d35825e969abe25d346af3feaeef725bef88575d
Peter Hagen <pha@hey.com>
2022-03-01 12:28:20 +0100

    Add basic page style modification

--- source/content.js
+++ source/content.js
@@ -1,15 +1,18 @@
-import optionsStorage from './options-storage.js';
+import optionsStorage from "./options-storage.js";
 
 async function init() {
-	const options = await optionsStorage.getAll();
-	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
-	const text = options.text;
-	const notice = document.createElement('div');
-	notice.innerHTML = text;
+	// const options = await optionsStorage.getAll();
+
+	const notice = document.createElement("div");
+	notice.innerHTML = "Sidebar";
 	document.body.append(notice);
-	notice.id = 'text-notice';
-	notice.style.border = '2px solid ' + color;
-	notice.style.color = color;
+	notice.className = "sidebar";
+}
+
+function addStyles(element, styles) {
+	for (id in styles) {
+		element.style[id] = styles[id];
+	}
 }
 
 init();
