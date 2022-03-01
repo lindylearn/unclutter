2b10cb29f1447267539b758ce865f7ef993991cc
Peter Hagen <pha@hey.com>
2022-03-01 14:21:22 +0100

    Add basic ease-in animation

--- source/background.js
+++ source/background.js
@@ -1,2 +1,11 @@
 // eslint-disable-next-line import/no-unassigned-import
-import './options-storage.js';
+import "./options-storage.js";
+
+chrome.browserAction.onClicked.addListener((tab) => {
+	chrome.tabs.insertCSS(tab.id, {
+		file: "content.css",
+	});
+	chrome.tabs.executeScript(tab.id, {
+		file: "content.js",
+	});
+});
--- source/content.js
+++ source/content.js
@@ -3,16 +3,12 @@ import optionsStorage from "./options-storage.js";
 async function init() {
 	// const options = await optionsStorage.getAll();
 
+	document.body.classList.add("pageview");
+
 	const notice = document.createElement("div");
-	notice.innerHTML = "Sidebar";
+	notice.innerHTML = "sdsd";
 	document.body.append(notice);
 	notice.className = "sidebar";
-}
-
-function addStyles(element, styles) {
-	for (id in styles) {
-		element.style[id] = styles[id];
-	}
 }
 
 init();
