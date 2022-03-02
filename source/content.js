2ef13b33232b9a31ec2a74322923aa4ecb2b753e
Peter Hagen <pha@hey.com>
2022-03-02 15:54:42 +0100

    Pass params to sidebar app

--- source/content.js
+++ source/content.js
@@ -23,8 +23,11 @@ async function togglePageView() {
 }
 
 function injectSidebar() {
+	const iframeUrl = new URL(browser.runtime.getURL('/sidebar/index.html'));
+	iframeUrl.searchParams.append('url', window.location.href);
+
 	const sidebarIframe = document.createElement('iframe');
-	sidebarIframe.src = browser.runtime.getURL('/sidebar/index.html');
+	sidebarIframe.src = iframeUrl.toString();
 	sidebarIframe.className = 'sidebar';
 	sidebarIframe.setAttribute('id', 'lindylearn-annotations-sidebar');
 	sidebarIframe.setAttribute('scrolling', 'no');
--- source/sidebar/index.js
+++ source/sidebar/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+const urlParams = new URLSearchParams(document.location.search);
 const domContainer = document.querySelector('#react-root');
-ReactDOM.render(<App />, domContainer);
+ReactDOM.render(<App {...Object.fromEntries(urlParams)} />, domContainer);
