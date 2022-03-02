bb49ab5366f7ee15c11c13205c2148126c9305f2
Peter Hagen <pha@hey.com>
2022-03-02 15:03:29 +0100

    Add tailwind to sidebar app

---
+++ source/sidebar/App.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+
+export default function App({}) {
+	const [annotations, setAnnotations] = useState([]);
+
+	return <div className="font-bold text-xl">Hey!</div>;
+}
--- source/sidebar/index.js
+++ source/sidebar/index.js
@@ -1,5 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const domContainer = document.querySelector("#react-root");
-ReactDOM.render(<div>Hello!!</div>, domContainer);
+import App from './App';
+
+const domContainer = document.querySelector('#react-root');
+ReactDOM.render(<App />, domContainer);
