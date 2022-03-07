abb7ab9ee53c5f013155690bb8c87d0719d9dfe7
Peter Hagen <pha@hey.com>
2022-03-07 13:01:50 +0100

    Try fix build error

--- source/options/index.js
+++ source/options/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Options from "./Options";
+import Options from "./options";
 
 const domContainer = document.querySelector("#react-root");
 ReactDOM.render(<Options />, domContainer);
