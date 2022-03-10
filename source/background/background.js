f278b89ff8f8771732d14293cdc5950bde83f1a2
Peter Hagen <pha@hey.com>
2022-03-10 17:09:12 +0100

    Add v2 manifest compatibility

--- source/background/background.js
+++ source/background/background.js
@@ -1,7 +1,7 @@
 import browser from "webextension-polyfill";
 
 // when extension icon clicked, inject annotations view into active tab
-browser.action.onClicked.addListener(async (tab) => {
+(browser.action || browser.browserAction).onClicked.addListener(async (tab) => {
     let alreadyInjected = false;
     try {
         alreadyInjected = await browser.tabs.sendMessage(tab.id, "ping");
@@ -12,14 +12,23 @@ browser.action.onClicked.addListener(async (tab) => {
     }
     if (!alreadyInjected) {
         console.log("Content script not loaded in active tab, injecting it...");
-        await browser.scripting.executeScript({
-            target: {
-                tabId: tab.id,
-            },
-            files: ["content-script/index.js"],
-        });
+        await _executeScript(tab.id, "content-script/index.js");
     }
 
     // toggle the page view
     await browser.tabs.sendMessage(tab.id, "togglePageView");
 });
+
+async function _executeScript(tabId, filePath) {
+    // different calls for v2 and v3 manifest
+    if (browser.scripting) {
+        await browser.scripting.executeScript({
+            target: { tabId },
+            files: [filePath],
+        });
+    } else {
+        await browser.tabs.executeScript(tabId, {
+            file: filePath,
+        });
+    }
+}
