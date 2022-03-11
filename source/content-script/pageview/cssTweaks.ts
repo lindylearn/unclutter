2353e59556ea14bc25fa3a1a68195d4870167e11
Peter Hagen <pha@hey.com>
2022-03-12 00:38:27 +0100

    Use postcss for css rewrites

--- source/content-script/pageview/cssTweaks.ts
+++ source/content-script/pageview/cssTweaks.ts
@@ -1,4 +1,4 @@
-import css from "css";
+import postcss, { Declaration, Plugin, PluginCreator } from "postcss";
 
 // The extension reduces the website body width to show annotations on the right side.
 // But since CSS media queries work on the actual viewport width, responsive style doesn't take this reduced body width into account.
@@ -8,139 +8,83 @@ export async function getCssOverride(
     cssText: string,
     conditionScale: number
 ): Promise<string> {
-    const rules = getRules(cssText);
-    if (!rules) {
-        return;
-    }
+    const start = performance.now();
+    const result = await postcss([
+        scaleBreakpointsPlugin(conditionScale),
+        urlRewritePlugin(cssUrl),
+        hideFixedElementsPlugin,
+    ]).process(cssText);
 
-    const baseRules = rules
-        .filter((rule: any) => rule.type !== "media")
-        .map((r) => r.text);
+    const duration = performance.now() - start;
+    console.log(`Took ${Math.round(duration)}ms to rewrite ${cssUrl}`);
 
-    /**
-     * Create override media rules upscaled to the entire page.
-     * Assuming scale factor 2 (page now takes up only 50% of the screen):
-     *     max-width 1 -> max-width 2
-     *     min-width 1 -> min-width 2
-     *         since we remove the old style, no need to negate old [1, 2] range
-     */
-    const overrideRules = rules
-        .filter((rule: any) => rule.type === "media")
-        .map((rule: any) => {
-            let newCondition = rule.condition;
+    // console.log(cssUrl, result.css);
 
-            for (const match of newCondition.matchAll(/([0-9]+)/g)) {
+    return result.css;
+}
+
+/**
+ * Create override media rules upscaled to the entire page.
+ * Assuming scale factor 2 (page now takes up only 50% of the screen):
+ *     max-width 1 -> max-width 2
+ *     min-width 1 -> min-width 2
+ *         since we remove the old style, no need to negate old [1, 2] range
+ */
+const scaleBreakpointsPlugin: PluginCreator<number> = (conditionScale) => ({
+    postcssPlugin: "scale-media-breakpoints",
+    AtRule(rule) {
+        if (rule.name === "media") {
+            let condition = rule.params;
+            for (const match of condition.matchAll(/([0-9]+)/g)) {
                 const oldCount = parseInt(match[1]);
                 const newCount = Math.round(conditionScale * oldCount);
 
-                newCondition = newCondition.replace(oldCount, newCount);
+                condition = condition.replace(
+                    oldCount.toString(),
+                    newCount.toString()
+                );
             }
 
-            let newText = rule.text.replace(rule.condition, newCondition);
-            if (rule.wrappedBySelector) {
-                newText = `${rule.wrappedBySelector} { ${newText} }`;
+            rule.params = condition;
+        }
+    },
+});
+scaleBreakpointsPlugin.postcss = true;
+
+// use absolute paths for files referenced via url()
+// those paths are relative to the stylesheet url, which we change
+const urlRewritePlugin: PluginCreator<string> = (baseUrl) => ({
+    postcssPlugin: "rewrite-relative-urls",
+    Declaration(decl) {
+        if (decl.value.startsWith("url(")) {
+            for (const match of decl.value.matchAll(
+                /url\((?!"?'?(?:data:|#|https?:\/\/))"?'?([^\)]*?)"?'?\)/g
+            )) {
+                const url = match[1];
+
+                const absoluteUrl = new URL(url, baseUrl);
+                decl.value = decl.value.replace(url, absoluteUrl.href);
             }
-            return newText;
-        })
-        .filter((text: string) => text);
+        }
+    },
+});
+urlRewritePlugin.postcss = true;
 
-    let newText = baseRules
-        .concat(overrideRules)
-        .map((text) => modifyRulesText(text, cssUrl))
-        .join("\n\n");
-
-    // console.log(cssUrl, newText);
-
-    // construct one override document per CSS document
-    return newText;
-}
-
-function getRules(text: string) {
-    const rules = css.parse(text, { silent: true })?.stylesheet?.rules;
-    if (!rules) {
-        return;
-    }
-
-    const textLines = text.split("\n");
-    const mappedRules = rules.map((rule: any) =>
-        mapRuleFormat(rule, textLines)
-    );
-
-    // rules wrapped by non-media rules, e.g. @supports (...) {}
-    // assume only one level nesting
-    const nonWrappedRules = mappedRules.filter(
-        (rule: any) => !rule.rules || rule.type === "media"
-    );
-    const wrappedRules = mappedRules
-        .filter((rule: any) => rule.rules && rule.type !== "media")
-        .flatMap((wrapper) => {
-            const wrappedBySelector = wrapper.text.match(/(.*?)\{/)[1];
-
-            return wrapper.rules.map((rule) => {
-                const mappedRule = mapRuleFormat(rule, textLines);
-                return {
-                    ...mappedRule,
-                    wrappedBySelector,
-                };
-            });
-        });
-
-    return nonWrappedRules.concat(wrappedRules);
-}
-
-function mapRuleFormat(rule: any, completeTextLines: string[]) {
-    let text = "";
-    if (rule.position.start.line === rule.position.end.line) {
-        text = completeTextLines[rule.position.start.line - 1].slice(
-            rule.position.start.column - 1,
-            rule.position.end.column - 1
-        );
-    } else {
-        text =
-            completeTextLines[rule.position.start.line - 1].slice(
-                rule.position.start.column - 1
-            ) +
-            "\n" +
-            completeTextLines
-                .slice(rule.position.start.line, rule.position.end.line - 1)
-                .join("\n") +
-            "\n" +
-            completeTextLines[rule.position.end.line - 1].slice(
-                0,
-                rule.position.end.column - 1
+// hide fixed and sticky positioned elements (highly unlikely to be part of the text)
+const hideFixedElementsPlugin: Plugin = {
+    postcssPlugin: "hide-fixed-elements",
+    Declaration(decl) {
+        if (
+            decl.prop === "position" &&
+            (decl.value === "fixed" || decl.value === "sticky")
+        ) {
+            decl.parent.append(
+                new Declaration({
+                    prop: "display",
+                    value: "none",
+                    important: true,
+                })
             );
-    }
-
-    return {
-        type: rule.type,
-        condition: rule.media as string,
-        selectors: rule.selectors,
-        rules: rule.rules,
-        text,
-    };
-}
-
-function modifyRulesText(text, cssUrl) {
-    // use absolute paths for files referenced via url()
-    // those paths are relative to the stylesheet url, which we change
-    for (const match of text.matchAll(
-        /url\((?!"?'?(?:data:|#|https?:\/\/))"?'?([^\)]*?)"?'?\)/g
-    )) {
-        const url = match[1];
-
-        const absoluteUrl = new URL(url, cssUrl);
-        text = text.replace(url, absoluteUrl.href);
-    }
-
-    // hide fixed and sticky positioned elements (highly unlikely to be part of the text)
-    text = text.replaceAll(
-        /position:\s?fixed\s?(\!important)?/g,
-        "position: fixed; display: none !important"
-    );
-    text = text.replaceAll(
-        /position:\s?sticky\s?(\!important)?/g,
-        "position: sticky; display: none !important"
-    );
-
-    return text;
-}
+        }
+    },
+};
