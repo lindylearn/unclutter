387ba01c99aa0c6c32265c37e095a8a2d86413e4
Peter Hagen <pha@hey.com>
2022-03-14 19:30:04 +0100

    Remove annotations api methods for now

--- source/common/storage.js
+++ source/common/storage.js
@@ -1,52 +1,5 @@
-import axios from "axios";
 import browser from "webextension-polyfill";
 import { defaultExcludedDomains } from "./defaultStorage";
-
-export async function getHypothesisToken() {
-    return (await getUserInfo())["hypothesis-api-token"];
-}
-
-export async function getHypothesisUsername() {
-    return (await getUserInfo())["hypothesis-username"];
-}
-
-export async function getUserInfo() {
-    return await browser.storage.sync.get([
-        "hypothesis-api-token",
-        "hypothesis-username",
-    ]);
-}
-
-export async function validateSaveToken(token, forceSave = false) {
-    const userName = await validateApiToken(token);
-    if (!userName && !forceSave) {
-        return false;
-    }
-
-    await browser.storage.sync.set({
-        "hypothesis-api-token": token,
-        "hypothesis-username": userName,
-    });
-
-    return true;
-}
-
-export async function validateApiToken(apiToken) {
-    try {
-        const response = await axios.get(
-            `https://api.hypothes.is/api/profile`,
-            {
-                headers: {
-                    Authorization: `Bearer ${apiToken}`,
-                },
-            }
-        );
-        const fullUserId = response.data.userid; // e.g. acct:remikalir@hypothes.is
-        return fullUserId.match(/([^:]+)@/)[1];
-    } catch {
-        return null;
-    }
-}
 
 export async function shouldEnableForDomain(domain) {
     const config = await browser.storage.sync.get([
