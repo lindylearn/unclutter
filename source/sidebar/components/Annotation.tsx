032d6ad796a5feb6b981dacb04b114a5dcf0d23f
Peter Hagen <pha@hey.com>
2022-04-19 16:47:25 +0200

    Add reply button

--- source/sidebar/components/Annotation.tsx
+++ source/sidebar/components/Annotation.tsx
@@ -41,13 +41,13 @@ function Annotation({
             >
                 {textLines.map((item, i) => {
                     return (
-                        <p key={i} className="">
+                        <p key={i} className="mb-1">
                             {item}
                             {i == textLines.length - 1 &&
                             text.length > charLimit ? (
                                 "..."
                             ) : (
-                                <br className="mb-1" />
+                                <br className="" />
                             )}
                         </p>
                     );
@@ -73,28 +73,25 @@ function Annotation({
                     </svg>
                     <span>{upvoteCount}</span>
                 </div>
-                {reply_count && reply_count > 0 ? (
-                    <a
-                        className="invisible"
-                        href={link}
-                        target="_blank"
-                        rel="noreferrer"
+
+                <a
+                    className="reply-button invisible select-none hover:text-gray-600 hover:scale-110 transition-all"
+                    href={link}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <svg
+                        className="inline-block align-baseline rotate-180 w-3 mr-1"
+                        viewBox="0 0 512 512"
+                        style={{ marginBottom: "-1px" }}
                     >
-                        <svg
-                            className="inline-block align-baseline text-gray-400 rotate-180 w-3 mr-1"
-                            viewBox="0 0 512 512"
-                            style={{ marginBottom: "-1px" }}
-                        >
-                            <path
-                                fill="currentColor"
-                                d="M8.309 189.836L184.313 37.851C199.719 24.546 224 35.347 224 56.015v80.053c160.629 1.839 288 34.032 288 186.258 0 61.441-39.581 122.309-83.333 154.132-13.653 9.931-33.111-2.533-28.077-18.631 45.344-145.012-21.507-183.51-176.59-185.742V360c0 20.7-24.3 31.453-39.687 18.164l-176.004-152c-11.071-9.562-11.086-26.753 0-36.328z"
-                            ></path>
-                        </svg>
-                        <span>{reply_count}</span>
-                    </a>
-                ) : (
-                    <></>
-                )}
+                        <path
+                            fill="currentColor"
+                            d="M8.309 189.836L184.313 37.851C199.719 24.546 224 35.347 224 56.015v80.053c160.629 1.839 288 34.032 288 186.258 0 61.441-39.581 122.309-83.333 154.132-13.653 9.931-33.111-2.533-28.077-18.631 45.344-145.012-21.507-183.51-176.59-185.742V360c0 20.7-24.3 31.453-39.687 18.164l-176.004-152c-11.071-9.562-11.086-26.753 0-36.328z"
+                        ></path>
+                    </svg>
+                    <span>reply</span>
+                </a>
 
                 <div className="flex-grow" />
                 <a
