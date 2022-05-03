894fd0e852dbd1193b8195882dd56de60dd44b00
Peter Hagen <pha@hey.com>
2022-05-03 15:27:16 +0200

    Cleanup outline style

--- source/overlay/outline/Outline.svelte
+++ source/overlay/outline/Outline.svelte
@@ -3,25 +3,26 @@
     import Heading from "./Heading.svelte";
     import ProgressCircle from './ProgressCircle.svelte';
     import { OutlineItem } from "./parse";
+    import { scrollToElement } from "./common";
 
     export let outline: OutlineItem[];
     export let activeOutlineIndex: number;
 </script>
 
 
-<div id="outline" class="max-w-full px-4 py-3 cursor-auto bg-white rounded-lg shadow hover:shadow-md transition-all relative">
+<div id="outline" class="max-w-full px-5 py-4 cursor-auto bg-white rounded-lg shadow hover:shadow-md transition-all relative">
     <div>
-        <div>
-            <div class="text-sm font-header font-semibold cursor-pointer pr-8">
+        <div class="flex justify-between">
+            <div class="text-sm font-header font-semibold cursor-pointer" on:click={() => scrollToElement(outline[0].element)}>
                 {outline[0].title}
             </div>
-            <div class="absolute top-3 right-3">
+            <div class="-mr-1">
                 <ProgressCircle progress={60} caption={'2'} />
             </div>
         </div>
-        <div class="border-b-2 decoration-gray-200 -mx-4 my-2"></div>
+        <div class="border-b-2 border-gray-100 -mx-5 my-2"></div>
 
-        <ul class="m-0 p-0 pl-4 list-none flex flex-col gap-1">
+        <ul class="m-0 p-0 pr-2 list-none flex flex-col gap-1">
             {#each outline.slice(1) as child, i}
                 <Heading {...child} activeOutlineIndex={activeOutlineIndex} />
             {/each}
