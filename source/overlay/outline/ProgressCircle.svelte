64f5e040b7afbe7677b6983e601cdd8f761a0558
Peter Hagen <pha@hey.com>
2022-05-03 22:25:44 +0200

    Refactor progress circle to use conic gradient

--- source/overlay/outline/ProgressCircle.svelte
+++ source/overlay/outline/ProgressCircle.svelte
@@ -7,53 +7,34 @@
     export let progressPercentage: number;
     export let caption: string;
 
-    const radius = 20;
-    const stroke = 4;
-    
-    const normalizedRadius = radius - stroke;
-    const circumference = normalizedRadius * 2 * Math.PI;
-
     let strokeDashoffset: number;
-    $: strokeDashoffset = circumference - progressPercentage * circumference;
+    $: strokeDashoffset = 288.5 - 288.5 * progressPercentage;
 </script>
 
 <div class="relative font-header hover:drop-shadow-md">
     <svg
-        height={radius * 2}
-        width={radius * 2}
-        class="progress-circle"
+        viewBox="0 0 100 100"
+        class="progress-circle w-10"
     >
-        <defs>
-            <linearGradient id="myGradient" gradientTransform="">
-                <stop offset="5%"  stop-color="rgba(255, 23, 68, 0.6)" />
-                <stop offset="15%" stop-color="rgba(213, 0, 249, 0.6)" />
-                <stop offset="25%" stop-color="rgba(61, 90, 254, 0.6)" />
-                <stop offset="35%" stop-color="rgba(0, 176, 255, 0.6)" />
-                <stop offset="45%" stop-color="rgba(29, 233, 182, 0.6)" />
-                <stop offset="55%" stop-color="rgba(118, 255, 3, 0.6)" />
-                <stop offset="65%" stop-color="rgba(255, 234, 0, 0.6)" />
-                <stop offset="95%" stop-color="rgba(255, 145, 0, 0.6)" />
-            </linearGradient>
-        </defs>
-        <circle
-            class="placeholder"
-            stroke="#f3f4f6"
-            fill="white"
-            stroke-width={ stroke }
-            r={ normalizedRadius }
-            cx={ radius }
-            cy={ radius }
-        />
-        <circle
-            stroke="url('#myGradient')"
-            fill="transparent"
-            stroke-dasharray={ circumference + ' ' + circumference }
+      <defs>
+        <mask id="mask">
+          <path 
+            class="logoPath" 
+            id="logo-path"
             style={`stroke-dashoffset: ${strokeDashoffset}`}
-            stroke-width={ stroke }
-            r={ normalizedRadius }
-            cx={ radius }
-            cy={ radius }
-        />
+            d="M 50 96 a 46 46 0 0 1 0 -92 46 46 0 0 1 0 92" 
+          />
+        </mask>
+      </defs>
+      
+      <path 
+        class="placeholder" 
+        d="M 50 96 a 46 46 0 0 1 0 -92 46 46 0 0 1 0 92" 
+      />
+
+      <foreignObject class="logoWrap" mask="url(#mask)" x="0" y="0" width="100" height="100">
+        <div class="logoGradient"></div>
+      </foreignObject>
     </svg>
 
     <div class="absolute font-semibold select-none" style="top: 20%; left: 38%;">
@@ -63,19 +44,37 @@
 
 
 <style lang="postcss">
-    /* html, body {
-    background-color: #2962FF;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    position: relative;
-    } */
+  svg {
+    transform-origin: bottom;
+  }
 
-    circle {
-        transition: stroke-dashoffset 0.35s;
-        transform: rotate(-90deg);
-        transform-origin: 50% 50%;
-        stroke-linecap: round;
-    }
+  .logoWrap {
+    transform-origin: 50px 50px;
+  }
+
+  .logoGradient {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: conic-gradient(from 270deg, #ff4800 10%, #dfd902 35%, #20dc68, #0092f4, #da54d8 72% 75%, #ff4800 95%);
+  }
+
+  .logoPath {
+    stroke: white;
+    stroke-width: 12;
+    stroke-linecap: round;
+    fill: none;
+    transform-origin: 50px 50px;
+    transform: rotate(-180deg);
+    transition: stroke-dashoffset 0.35s;
+
+    stroke-dasharray: 288.5;
+  }
+
+  .placeholder {
+    stroke: #f3f4f6;
+    stroke-width: 6;
+    transform-origin: 50px 50px;
+  }
+
 </style>
