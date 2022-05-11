defffd15c09c0e7ae424f3b486442c2f791ddb3a
Peter Hagen <pha@hey.com>
2022-05-11 13:44:53 +0200

    Fix nested outline attribution

--- source/overlay/outline/Heading.svelte
+++ source/overlay/outline/Heading.svelte
@@ -65,7 +65,12 @@
     {#if children.length > 0}
         <ul class="m-0 ml-5 p-0 list-none mt-1 flex flex-col gap-1">
             {#each children as child, i}
-                <svelte:self {...child} activeOutlineIndex={activeOutlineIndex} />
+                <svelte:self 
+                    activeOutlineIndex={activeOutlineIndex}
+                    annotationsEnabled={annotationsEnabled}
+                    socialAnnotationsEnabled={socialAnnotationsEnabled}
+                    {...child}
+                />
             {/each}
         </ul>
     {/if}
