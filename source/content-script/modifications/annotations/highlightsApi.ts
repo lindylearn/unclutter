c90d59492f99fa9ae1c4248d1d066268fca53262
Peter Hagen <pha@hey.com>
2022-05-17 12:48:27 +0200

    Show annotation dots in sidebar for testing

--- source/content-script/modifications/annotations/highlightsApi.ts
+++ source/content-script/modifications/annotations/highlightsApi.ts
@@ -111,12 +111,15 @@ export async function highlightAnnotations(
         })
     );
 
-    // insertMarginBar(anchoredAnnotations)
+    insertMarginBar(anchoredAnnotations, sidebarIframe);
 
     return anchoredAnnotations;
 }
 
-function insertMarginBar(anchoredAnnotations) {
+function insertMarginBar(
+    anchoredAnnotations: LindyAnnotation[],
+    sidebarIframe: HTMLIFrameElement
+) {
     const container = document.createElement("div");
     container.className = overrideClassname;
     container.id = "lindy-annotations-marginbar";
@@ -130,23 +133,32 @@ function insertMarginBar(anchoredAnnotations) {
 
         const barElement = document.createElement("div");
 
-        barElement.style.top = `${annotation.displayOffset - bodyOffset}px`;
-        barElement.style.height = `${
-            annotation.displayOffsetEnd - annotation.displayOffset
+        barElement.className = "lindy-marginbar-dot";
+        barElement.style.top = `${
+            annotation.displayOffset - bodyOffset + 10
         }px`;
+        // barElement.style.height = `${
+        //     annotation.displayOffsetEnd - annotation.displayOffset
+        // }px`;
 
-        // barElement.innerText = annotation.reply_count || 1;
-        barElement.style.paddingRight = "5px";
-        barElement.style.color = "#9ca3af";
-        barElement.style.fontWeight = "700";
-        barElement.style.backgroundColor = "#ffb380"; // getAnnotationColor(annotation);
+        const annotationColor =
+            annotation.platform === "hn"
+                ? "rgba(255, 102, 0, 0.5)"
+                : "rgba(189, 28, 43, 0.5)";
+        const darkerAnnotationColor = annotationColor.replace("0.5", "0.8");
+        barElement.style.setProperty(
+            "--darker-annotation-color",
+            darkerAnnotationColor
+        );
 
-        // barElement.innerHTML = `<svg style="width: 20px" viewBox="0 0 512 512">
-        //     <path
-        //         fill="currentColor"
-        //         d="M256 32C114.6 32 .0272 125.1 .0272 240c0 47.63 19.91 91.25 52.91 126.2c-14.88 39.5-45.87 72.88-46.37 73.25c-6.625 7-8.375 17.25-4.625 26C5.818 474.2 14.38 480 24 480c61.5 0 109.1-25.75 139.1-46.25C191.1 442.8 223.3 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32zM256.1 400c-26.75 0-53.12-4.125-78.38-12.12l-22.75-7.125l-19.5 13.75c-14.25 10.12-33.88 21.38-57.5 29c7.375-12.12 14.37-25.75 19.88-40.25l10.62-28l-20.62-21.87C69.82 314.1 48.07 282.2 48.07 240c0-88.25 93.25-160 208-160s208 71.75 208 160S370.8 400 256.1 400z"
-        //     />
-        // </svg>`;
+        barElement.onmouseenter = () => {
+            hoverUpdateHighlight(annotation, true);
+
+            sendSidebarEvent(sidebarIframe, {
+                event: "focusAnnotation",
+                localId: annotation.localId,
+            });
+        };
 
         container.appendChild(barElement);
     });
