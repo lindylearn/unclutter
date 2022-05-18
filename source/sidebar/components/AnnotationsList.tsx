296cfcb7dd27dd4a086e1d10bd6eca76c5b940b4
Peter Hagen <pha@hey.com>
2022-05-18 13:41:35 +0200

    Render annotations in flat list and add transition group

--- source/sidebar/components/AnnotationsList.tsx
+++ source/sidebar/components/AnnotationsList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { LindyAnnotation } from "../../common/annotations/create";
 import AnnotationThread from "./AnnotationThread";
 
@@ -35,97 +36,62 @@ function AnnotationsList({
     updateAnnotation,
 }: AnnotationsListProps) {
     return (
-        <div className="relative flex-grow">
-            {groupedAnnotations.map((group, groupIndex) => (
-                <AnnotationGroup
-                    key={group[0].localId}
-                    group={group}
-                    nextGroup={
-                        groupIndex < groupedAnnotations.length - 1 &&
-                        groupedAnnotations[groupIndex + 1]
-                    }
-                    deleteHideAnnotation={deleteHideAnnotation}
-                    onAnnotationHoverUpdate={onAnnotationHoverUpdate}
-                    unfocusAnnotation={unfocusAnnotation}
-                    hypothesisSyncEnabled={hypothesisSyncEnabled}
-                    showAllSocialAnnotations={showAllSocialAnnotations}
-                    createReply={createReply}
-                    updateAnnotation={updateAnnotation}
-                />
-            ))}
-        </div>
-    );
-}
-export default AnnotationsList;
+        <TransitionGroup className="annotation-list relative flex-grow">
+            {/* render annotations in flat list to animate fade-out and regroups */}
+            {groupedAnnotations.flatMap((group, groupIndex) => {
+                const nextGroup =
+                    groupIndex < groupedAnnotations.length - 1 &&
+                    groupedAnnotations[groupIndex + 1];
 
-interface AnnotationGroupProps {
-    group: LindyAnnotation[];
-    nextGroup?: LindyAnnotation[];
-    hypothesisSyncEnabled: boolean;
-    showAllSocialAnnotations: boolean;
-    deleteHideAnnotation: (
-        annotation: LindyAnnotation,
-        threadStart: LindyAnnotation
-    ) => void;
-    onAnnotationHoverUpdate: (
-        annotation: LindyAnnotation,
-        hoverActive: boolean
-    ) => void;
-    unfocusAnnotation: (annotation: LindyAnnotation) => void;
-    createReply: (
-        parent: LindyAnnotation,
-        threadStart: LindyAnnotation
-    ) => void;
-    updateAnnotation: (annotation: LindyAnnotation) => void;
-}
-
-function AnnotationGroup({
-    group,
-    nextGroup,
-    hypothesisSyncEnabled,
-    showAllSocialAnnotations,
-    deleteHideAnnotation,
-    onAnnotationHoverUpdate,
-    unfocusAnnotation,
-    createReply,
-    updateAnnotation,
-}: AnnotationGroupProps) {
-    return (
-        <div
-            className="absolute w-full flex flex-col gap-1"
-            style={{
-                top: group[0].displayOffset - sidebarOffsetTopPx,
-            }}
-        >
-            {group.map((annotation) => {
-                const heightLimitPx =
+                const groupTopOffset =
+                    group[0].displayOffset - sidebarOffsetTopPx;
+                const groupHeightLimitPx =
                     (nextGroup?.[0]?.displayOffset ||
                         document.documentElement.scrollHeight +
                             sidebarOffsetTopPx) -
                     group[0].displayOffset -
                     4; // margin;
 
-                return (
-                    <div
-                        key={annotation.localId}
-                        className="annotation-group-item"
-                    >
-                        <AnnotationThread
-                            annotation={annotation}
-                            deleteHideAnnotation={deleteHideAnnotation}
-                            heightLimitPx={heightLimitPx / group.length} // give each item equal share -- always avoids overflows
-                            onHoverUpdate={(hoverActive: boolean) =>
-                                // call hover on top level annotation
-                                onAnnotationHoverUpdate(annotation, hoverActive)
-                            }
-                            unfocusAnnotation={unfocusAnnotation}
-                            hypothesisSyncEnabled={hypothesisSyncEnabled}
-                            createReply={createReply}
-                            updateAnnotation={updateAnnotation}
-                        />
-                    </div>
-                );
+                return group.map((annotation, i) => {
+                    return (
+                        <CSSTransition
+                            key={annotation.localId}
+                            timeout={500}
+                            classNames="annotation-list-item"
+                        >
+                            <div
+                                key={annotation.localId}
+                                className="absolute"
+                                style={{
+                                    top: groupTopOffset + 60 * i,
+                                }}
+                            >
+                                <AnnotationThread
+                                    annotation={annotation}
+                                    deleteHideAnnotation={deleteHideAnnotation}
+                                    heightLimitPx={
+                                        groupHeightLimitPx / group.length
+                                    } // give each item equal share -- always avoids overflows
+                                    onHoverUpdate={(hoverActive: boolean) =>
+                                        // call hover on top level annotation
+                                        onAnnotationHoverUpdate(
+                                            annotation,
+                                            hoverActive
+                                        )
+                                    }
+                                    unfocusAnnotation={unfocusAnnotation}
+                                    hypothesisSyncEnabled={
+                                        hypothesisSyncEnabled
+                                    }
+                                    createReply={createReply}
+                                    updateAnnotation={updateAnnotation}
+                                />
+                            </div>
+                        </CSSTransition>
+                    );
+                });
             })}
-        </div>
+        </TransitionGroup>
     );
 }
+export default AnnotationsList;
