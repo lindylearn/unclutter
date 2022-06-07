9fe923628cdcf8ed41008fc41f637b4233e40666
Peter Hagen <pha@hey.com>
2022-06-07 13:07:50 +0200

    Fix overlay icon flash before CSS rendered

--- source/common/html.ts
+++ source/common/html.ts
@@ -4,6 +4,7 @@ export function insertHtml(className: string, html: string) {
     const container = document.createElement("div");
     container.className = `${overrideClassname} ${className}`;
     container.style.contain = "layout style";
+    container.style.visibility = "hidden"; // hide until overlay/index.css applied
     container.id = className;
     container.innerHTML = html;
 
