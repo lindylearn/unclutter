8d305a08218edc06d5a17c324f3e51e43465c9e3
Peter Hagen <pha@hey.com>
2022-08-24 12:47:37 +0200

    Open related articles onClick and add fallback title

--- source/overlay/outline/Library/ArticlePreview.svelte
+++ source/overlay/outline/Library/ArticlePreview.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import clsx from "clsx";
 
+    import browser from "../../../common/polyfill";
     import { LibraryArticle } from "../../../common/schema";
 
     export let article: LibraryArticle;
@@ -10,16 +11,34 @@
         article.url
         // @ts-ignore
     ).replaceAll("%", "%25")}.webp`;
+
+    function openPage(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        browser.runtime.sendMessage(null, {
+            event: "openLinkWithUnclutter",
+            url: article.url,
+            newTab: true,
+        });
+    }
 </script>
 
-<div
+<a
     class={clsx(
         "relative h-52 w-44 flex-shrink-0 cursor-pointer rounded-lg bg-white transition-all dark:brightness-90 shadow hover:shadow-lg",
         index % 2 === 0 ? "hover:-rotate-2" : "hover:rotate-2"
     )}
+    href={article.url}
+    on:click={openPage}
 >
+    <div class="article-fallback p-3">
+        <div class="select-none font-bold leading-tight">
+            {article.title}
+        </div>
+    </div>
     <div
         class="absolute top-0 left-0 h-full w-full rounded-lg bg-cover"
         style={`background-image: url(${imageUrl});`}
     />
-</div>
+</a>
