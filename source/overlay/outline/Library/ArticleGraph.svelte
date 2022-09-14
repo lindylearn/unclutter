68023e5ef23f45fc9d6b2fe0bc617c8a6d9573af
Peter Hagen <pha@hey.com>
2022-09-14 17:14:12 +0200

    Patch force-graph to disregard page scroll

--- source/overlay/outline/Library/ArticleGraph.svelte
+++ source/overlay/outline/Library/ArticleGraph.svelte
@@ -19,14 +19,14 @@
         const height = 100;
 
         const forceGraph = ForceGraph()(graphContainer)
+            // layout
             .graphData({ nodes, links })
             .width(width)
             .height(height)
+            // simulation props
             .d3AlphaDecay(0.01)
             .d3VelocityDecay(0.08)
             // .warmupTicks(100)
-            // .nodeLabel("none")
-            // .onNodeClick((node, event) => window.open(node.id.toString()))
             // .cooldownTicks(0);
             .d3Force("center", (alpha) => {
                 nodes.forEach((node) => {
@@ -35,8 +35,6 @@
                     node.vx -= node.x * alpha * 0.05;
                 });
             })
-            // .d3Force("charge", null)
-            // .d3Force("link", null)
             .onEngineTick(() => {
                 // forces nodes inside bounding box
                 nodes.forEach((node) => {
@@ -51,12 +49,13 @@
                     );
                 });
             })
+            // styling
             .nodeColor(() => "rgba(0, 229, 255, 0.8)")
             .linkColor(() => "#9ca3af")
+            // interaction
             .enableZoomInteraction(false)
-            .enablePanInteraction(false);
-
-        // forceGraph.onEngineStop(() => forceGraph.zoomToFit(0));
+            .enablePanInteraction(false)
+            .onNodeClick((node, event) => window.open(node.id.toString()));
     }
 </script>
 
@@ -67,6 +66,6 @@
 
 <style lang="postcss" global>
     .graph-tooltip {
-        /* position: absolute; */
+        position: absolute;
     }
 </style>
