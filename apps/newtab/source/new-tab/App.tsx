09ccbc6a7d600376a2ddf7ed00015a1f0d0f1757
Peter Hagen <pha@hey.com>
2022-10-19 12:28:29 +0200

    Remove settings fetch in newtab app

--- apps/newtab/source/new-tab/App.tsx
+++ apps/newtab/source/new-tab/App.tsx
@@ -21,15 +21,12 @@ import {
     getWeekStart,
     getLocalScreenshot,
 } from "@unclutter/library-components/dist/common";
-
-import { settingsStore, useSettings } from "../common/settings";
+import NewTabModal from "./Modal";
 
 import "@unclutter/library-components/styles/globals.css";
 import "@unclutter/library-components/styles/ArticlePreview.css";
 import "@unclutter/library-components/styles/ProgressCircle.css";
 import "./app.css";
-import clsx from "clsx";
-import NewTabModal from "./Modal";
 
 export default function App() {
     // send messages to main Unclutter extension directly by passing its id
@@ -122,29 +119,6 @@ function ArticleSection({
     darkModeEnabled: boolean;
     setShowModal: (showModal: boolean) => void;
 }) {
-    // const tabInfos = useTabInfos(10, true, false, null, userInfo);
-    // const [articleListsCache, setArticleListsCache] =
-    //     useArticleListsCache(tabInfos);
-
-    // const settings = useSettings(settingsStore);
-    // const [initialIndex, setInitialIndex] = useState<number | null>();
-    // useLayoutEffect(() => {
-    //     if (tabInfos && settings) {
-    //         if (settings.newtabActiveGroupKey === null) {
-    //             setInitialIndex(null);
-    //             return;
-    //         }
-    //         const index = tabInfos?.findIndex(
-    //             (tab) => tab.key === settings.newtabActiveGroupKey
-    //         );
-    //         if (tabInfos[index]) {
-    //             setInitialIndex(index);
-    //         } else {
-    //             setInitialIndex(0);
-    //         }
-    //     }
-    // }, [tabInfos, settings]);
-
     const rep = useContext(ReplicacheContext);
 
     const [queuedArticles, setQueuedArticles] = useState<Article[]>([]);
