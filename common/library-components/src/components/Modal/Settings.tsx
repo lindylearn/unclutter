5a27e34149bd714d4775fc845e70bfa95268c858
Peter Hagen <pha@hey.com>
2022-10-20 15:03:17 +0200

    Fix showSignup feature flag

--- common/library-components/src/components/Modal/Settings.tsx
+++ common/library-components/src/components/Modal/Settings.tsx
@@ -68,18 +68,18 @@ export default function SettingsModalTab({
                 </p>
             </SettingsGroup>
 
-            <SettingsGroup
-                title="Account"
-                icon={
-                    <svg className="h-4 w-4" viewBox="0 0 448 512">
-                        <path
-                            fill="currentColor"
-                            d="M272 304h-96C78.8 304 0 382.8 0 480c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32C448 382.8 369.2 304 272 304zM48.99 464C56.89 400.9 110.8 352 176 352h96c65.16 0 119.1 48.95 127 112H48.99zM224 256c70.69 0 128-57.31 128-128c0-70.69-57.31-128-128-128S96 57.31 96 128C96 198.7 153.3 256 224 256zM224 48c44.11 0 80 35.89 80 80c0 44.11-35.89 80-80 80S144 172.1 144 128C144 83.89 179.9 48 224 48z"
-                        />
-                    </svg>
-                }
-            >
-                {userInfo.accountEnabled ? (
+            {userInfo.accountEnabled && (
+                <SettingsGroup
+                    title="Account"
+                    icon={
+                        <svg className="h-4 w-4" viewBox="0 0 448 512">
+                            <path
+                                fill="currentColor"
+                                d="M272 304h-96C78.8 304 0 382.8 0 480c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32C448 382.8 369.2 304 272 304zM48.99 464C56.89 400.9 110.8 352 176 352h96c65.16 0 119.1 48.95 127 112H48.99zM224 256c70.69 0 128-57.31 128-128c0-70.69-57.31-128-128-128S96 57.31 96 128C96 198.7 153.3 256 224 256zM224 48c44.11 0 80 35.89 80 80c0 44.11-35.89 80-80 80S144 172.1 144 128C144 83.89 179.9 48 224 48z"
+                            />
+                        </svg>
+                    }
+                >
                     <>
                         <p>
                             You're signed in as {userInfo.email}.{" "}
@@ -88,7 +88,21 @@ export default function SettingsModalTab({
                             )}
                         </p>
                     </>
-                ) : (
+                </SettingsGroup>
+            )}
+
+            {showSignup && (
+                <SettingsGroup
+                    title="Account"
+                    icon={
+                        <svg className="h-4 w-4" viewBox="0 0 448 512">
+                            <path
+                                fill="currentColor"
+                                d="M272 304h-96C78.8 304 0 382.8 0 480c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32C448 382.8 369.2 304 272 304zM48.99 464C56.89 400.9 110.8 352 176 352h96c65.16 0 119.1 48.95 127 112H48.99zM224 256c70.69 0 128-57.31 128-128c0-70.69-57.31-128-128-128S96 57.31 96 128C96 198.7 153.3 256 224 256zM224 48c44.11 0 80 35.89 80 80c0 44.11-35.89 80-80 80S144 172.1 144 128C144 83.89 179.9 48 224 48z"
+                            />
+                        </svg>
+                    }
+                >
                     <>
                         <p>
                             Right now, articles you visit are only saved in your browser. Create an
@@ -102,8 +116,8 @@ export default function SettingsModalTab({
                             />
                         </div>
                     </>
-                )}
-            </SettingsGroup>
+                </SettingsGroup>
+            )}
 
             {/* <SettingsGroup
                 title="Customization"
