a0a0ec4d88d6d3310fd0e980bddbc28c7d76e0e4
Peter Hagen <pha@hey.com>
2022-10-26 17:43:10 +0200

    Use new colors for annotations

--- apps/unclutter/source/common/annotations/styling.tsx
+++ apps/unclutter/source/common/annotations/styling.tsx
@@ -3,22 +3,22 @@ import { LindyAnnotation } from "./create";
 
 // material UI colors A400 from https://materialui.co/colors/, with 30% opacity
 export const colors = [
-    "rgba(255, 23, 68, 0.3)",
-    "rgba(245, 0, 87, 0.3)",
-    "rgba(213, 0, 249, 0.3)",
-    "rgba(101, 31, 255, 0.3)",
-    "rgba(61, 90, 254, 0.3)",
-    "rgba(41, 121, 255, 0.3)",
-    "rgba(0, 176, 255, 0.3)",
-    "rgba(0, 229, 255, 0.3)",
-    "rgba(29, 233, 182, 0.3)",
-    "rgba(0, 230, 118, 0.3)",
-    "rgba(118, 255, 3, 0.3)",
-    "rgba(198, 255, 0, 0.3)",
-    "rgba(255, 234, 0, 0.3)",
-    "rgba(255, 196, 0, 0.3)",
-    "rgba(255, 145, 0, 0.3)",
-    "rgba(255, 61, 0, 0.3)",
+    "rgba(255, 138, 128, 1.0)",
+    "rgba(255, 128, 171, 1.0)",
+    "rgba(234, 128, 252, 1.0)",
+    "rgba(179, 136, 255, 1.0)",
+    "rgba(140, 158, 255, 1.0)",
+    "rgba(130, 177, 255, 1.0)",
+    "rgba(128, 216, 255, 1.0)",
+    "rgba(132, 255, 255, 1.0)",
+    "rgba(167, 255, 235, 1.0)",
+    "rgba(185, 246, 202, 1.0)",
+    "rgba(204, 255, 144, 1.0)",
+    // "rgba(244, 255, 129, 1.0)",
+    // "rgba(255, 255, 141, 1.0)",
+    "rgba(255, 229, 127, 1.0)",
+    "rgba(255, 209, 128, 1.0)",
+    "rgba(255, 158, 128, 1.0)",
 ];
 
 export function getAnnotationColor(annotation: LindyAnnotation): string {
@@ -39,17 +39,4 @@ function randomInRange(seed, min, max) {
 
 function drawFromArray(seed, arr) {
     return arr[randomInRange(seed, 0, arr.length)];
-}
-
-export function ColoredText({ text, className = "" }) {
-    return (
-        <span
-            className={"leading-none " + className}
-            style={{
-                backgroundColor: getAnnotationColor({ quote_text: text }),
-            }}
-        >
-            {text}
-        </span>
-    );
 }
