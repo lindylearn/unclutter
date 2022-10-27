18f0932b4363233ca52a4b2be01b54b7db58490b
Peter Hagen <pha@hey.com>
2022-10-27 19:04:00 +0200

    Fix highlight dropdown rounding

--- common/library-components/src/components/Dropdown/ArticleDropdown.tsx
+++ common/library-components/src/components/Dropdown/ArticleDropdown.tsx
@@ -105,6 +105,7 @@ export function ArticleDropdown({
                     </svg>
                 }
                 onSelect={toggleQueued}
+                top
             />
 
             <DropdownItem
--- common/library-components/src/components/Dropdown/HighlightDowndown.tsx
+++ common/library-components/src/components/Dropdown/HighlightDowndown.tsx
@@ -74,6 +74,7 @@ export function HighlightDropdown({
                     </svg>
                 }
                 onSelect={deleteAnnotation}
+                bottom
             />
         </Dropdown>
     );
