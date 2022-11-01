08e2c0aaaf79041ec67c6b345a42a3da77d5999b
Peter Hagen <pha@hey.com>
2022-11-01 10:01:12 +0100

    Rename "report bug" to "give feedback"

--- common/library-components/src/components/Modal/Settings.tsx
+++ common/library-components/src/components/Modal/Settings.tsx
@@ -246,7 +246,7 @@ export default function SettingsModalTab({
                         reportEvent={reportEvent}
                     />
                     <Button
-                        title="Report issue"
+                        title="Give feedback"
                         onClick={() => setBugReportOpen(!bugReportOpen)}
                         darkModeEnabled={darkModeEnabled}
                         reportEvent={reportEvent}
@@ -257,12 +257,12 @@ export default function SettingsModalTab({
             {bugReportOpen && (
                 <div className="relative -translate-y-[100%]">
                     <SettingsGroup
-                        title="Report issue"
+                        title="Give feedback"
                         icon={
                             <svg className="h-4 w-4" viewBox="0 0 512 512">
                                 <path
                                     fill="currentColor"
-                                    d="M352 96V99.56C352 115.3 339.3 128 323.6 128H188.4C172.7 128 160 115.3 160 99.56V96C160 42.98 202.1 0 256 0C309 0 352 42.98 352 96zM39.03 103C48.4 93.66 63.6 93.66 72.97 103L145.4 175.5C161.3 165.7 179.1 160 200 160H312C332 160 350.7 165.7 366.6 175.5L439 103C448.4 93.66 463.6 93.66 472.1 103C482.3 112.4 482.3 127.6 472.1 136.1L400.5 209.4C410.3 225.3 416 243.1 416 264H488C501.3 264 512 274.7 512 288C512 301.3 501.3 312 488 312H416V320C416 347.2 409.2 372.8 397.2 395.3L472.1 471C482.3 480.4 482.3 495.6 472.1 504.1C463.6 514.3 448.4 514.3 439 504.1L368.2 434.1C339.3 462.5 299.7 480 256 480C212.3 480 172.7 462.5 143.8 434.1L72.97 504.1C63.6 514.3 48.4 514.3 39.03 504.1C29.66 495.6 29.66 480.4 39.03 471L114.8 395.3C102.8 372.8 96 347.2 96 320V312H24C10.75 312 0 301.3 0 288C0 274.7 10.75 264 24 264H96C96 243.1 101.7 225.3 111.5 209.4L39.03 136.1C29.66 127.6 29.66 112.4 39.03 103V103zM144 320C144 373.6 181.7 418.4 232 429.4V280C232 266.7 242.7 256 256 256C269.3 256 280 266.7 280 280V429.4C330.3 418.4 368 373.6 368 320V264C368 233.1 342.9 208 312 208H200C169.1 208 144 233.1 144 264V320z"
+                                    d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z"
                                 />
                             </svg>
                         }
@@ -270,7 +270,7 @@ export default function SettingsModalTab({
                     >
                         <textarea
                             className="my-1 h-32 w-full rounded-md p-3 placeholder-neutral-400 outline-none dark:bg-[#212121] dark:placeholder-neutral-500"
-                            placeholder="What is wrong or could work better?"
+                            placeholder="What do you think about Unclutter? What could work better?"
                             ref={messageRef}
                         />
                         <div className="flex justify-end gap-3">
