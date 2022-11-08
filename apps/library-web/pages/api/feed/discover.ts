7ead4c54c2221ee739b57c315ab5dd58d8ec8199
Peter Hagen <pha@hey.com>
2022-11-08 20:48:37 +0100

    Fix build

--- apps/library-web/pages/api/feed/discover.ts
+++ apps/library-web/pages/api/feed/discover.ts
@@ -1,18 +1,18 @@
 import { JSDOM } from "jsdom";
-import { discoverFeeds, getMainFeed } from "@unclutter/library-components/dist/feeds";
+// import { discoverFeeds, getMainFeed } from "@unclutter/library-components/dist/feeds";
 
 export default async function handler(req, res) {
     const sourceUrl = req.query.url;
 
     const html = await fetch(sourceUrl).then((response) => response.text());
-    const document: Document = new JSDOM(html).window.document;
-    if (!document) {
-        console.error("Invalid HTML");
-        return [];
-    }
+    // const document: Document = new JSDOM(html).window.document;
+    // if (!document) {
+    //     console.error("Invalid HTML");
+    //     return [];
+    // }
 
-    const feedUrls = await discoverFeeds(document, sourceUrl);
-    const feed = await getMainFeed(sourceUrl, feedUrls);
+    // const feedUrls = await discoverFeeds(document, sourceUrl);
+    // const feed = await getMainFeed(sourceUrl, feedUrls);
 
-    res.status(200).json(feed);
+    // res.status(200).json(feed);
 }
