6b53992e63b8255c3ae0b68124cc23e09222ff11
Peter Hagen <pha@hey.com>
2022-11-14 14:24:24 +0100

    Change toggle message icon on hover

--- apps/unclutter/source/overlay/outline/Library/ToggleMessage.svelte
+++ apps/unclutter/source/overlay/outline/Library/ToggleMessage.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import { fly } from "svelte/transition";
-    import { cubicOut } from "svelte/easing";
+    import { fly, fade } from "svelte/transition";
+    import { cubicOut, cubicIn } from "svelte/easing";
     import clsx from "clsx";
 
     export let inactiveColor: string;
@@ -36,14 +36,29 @@
 
     <div
         class={clsx(
-            "toggle transition-color flex h-[calc(1rem+0.5rem+1.25rem+0.75rem*2)] shrink-0 origin-left items-center rounded-r-lg px-3",
+            "toggle transition-color flex h-[calc(1rem+0.5rem+1.25rem+0.75rem*2)] shrink-0 origin-left items-center rounded-r-lg px-3 w-12 relative",
             isActive ? "active" : "inactive",
             darkModeEnabled && "dark"
         )}
         style={`--inactive-color: ${inactiveColor}; --active-color: ${activeColor};`}
         on:click={onToggle}
     >
-        <slot name="toggle-icon" />
+        <div
+            class="active-icon absolute top-0 left-0 flex h-full w-full items-center justify-center opacity-100"
+        >
+            <slot name="toggle-icon" />
+        </div>
+
+        <div
+            class="inactive-icon absolute top-0 left-0 flex h-full w-full items-center justify-center opacity-100"
+        >
+            <svg class="mx-0.5 w-5" viewBox="0 0 448 512">
+                <path
+                    fill="currentColor"
+                    d="M432 256C432 269.3 421.3 280 408 280h-160v160c0 13.25-10.75 24.01-24 24.01S200 453.3 200 440v-160h-160c-13.25 0-24-10.74-24-23.99C16 242.8 26.75 232 40 232h160v-160c0-13.25 10.75-23.99 24-23.99S248 58.75 248 72v160h160C421.3 232 432 242.8 432 256z"
+                />
+            </svg>
+        </div>
     </div>
 </div>
 
@@ -64,5 +79,21 @@
     }
     .toggle.inactive:hover {
         background: var(--active-color) !important;
+    }
+
+    .toggle > * {
+        transition: opacity 0.2s ease-in-out !important;
+    }
+    .toggle.inactive > .active-icon {
+        opacity: 0 !important;
+    }
+    .toggle.active > .inactive-icon {
+        opacity: 0 !important;
+    }
+    .toggle.inactive:hover > .inactive-icon {
+        opacity: 0 !important;
+    }
+    .toggle.inactive:hover > .active-icon {
+        opacity: 1 !important;
     }
 </style>
