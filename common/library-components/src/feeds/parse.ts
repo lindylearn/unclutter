68bbd5108f26e160ad9c52e7c6e82408882b493a
Peter Hagen <pha@hey.com>
2022-12-09 10:23:04 +0100

    Fix rss parsing error

--- common/library-components/src/feeds/parse.ts
+++ common/library-components/src/feeds/parse.ts
@@ -120,6 +120,9 @@ export function parseFeedArticles(
 
     return items.map((item) => {
         let url = item.link!;
+        if (!url) {
+            url = `https://${getDomain(rssUrl)}`;
+        }
         if (url.startsWith("/")) {
             url = new URL(url, new URL(rssUrl).origin).href;
         }
