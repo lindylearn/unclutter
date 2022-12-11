75e4edc19b90aea50f226f41c44a23768fbf811d
Peter Hagen <pha@hey.com>
2022-12-11 14:52:57 +0100

    Use mock review stats data

--- common/library-components/src/components/Review/ReviewChart.tsx
+++ common/library-components/src/components/Review/ReviewChart.tsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useMemo, useState } from "react";
 import { ResponsiveStream, StreamDatum } from "@nivo/stream";
-import { Article, ReplicacheContext } from "../../store";
-import { getWeekNumber } from "../../common";
+import { Annotation, Article, ReplicacheContext } from "../../store";
+import { getRandomLightColor, getWeekNumber, lightColors } from "../../common";
+import { eachDayOfInterval, subWeeks } from "date-fns";
 
 export function ReviewChart({}: {}) {
     const rep = useContext(ReplicacheContext);
@@ -13,35 +14,48 @@ export function ReviewChart({}: {}) {
             return;
         }
 
-        rep.query.listArticles().then((articles) => {
-            const [data, keys] = createTopicStreamData(articles);
+        rep.query.listAnnotations().then((annotations) => {
+            let [data, keys] = createStreamData(annotations);
+
+            data = data.reverse();
+            data = data.slice(3, 4).concat(data.slice(0, 3)).concat(data.slice(4));
+
             setData(data);
             setKeys(keys);
         });
     }, [rep]);
 
     return (
-        <div className="h-56 w-full">
+        <div className="h-40 w-full">
             {/* see https://nivo.rocks/stream/ */}
             <ResponsiveStream
                 data={data || []}
                 keys={keys || []}
-                margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                margin={{ top: 20, right: 0, bottom: 0, left: 0 }}
                 axisTop={null}
                 axisRight={null}
-                axisBottom={null}
+                axisBottom={{
+                    format: (value) => value,
+                }}
                 axisLeft={null}
                 enableGridX={false}
                 enableGridY={false}
                 offsetType="diverging"
-                colors={{ scheme: "nivo" }}
-                fillOpacity={0.85}
+                // colors={{ scheme: "nivo" }}
+                // colors={lightColors}
+                colors={[
+                    getRandomLightColor("writing"),
+                    getRandomLightColor("note-taking"),
+                    getRandomLightColor("reading"),
+                ]}
+                fillOpacity={0.8}
                 borderColor={{ theme: "background" }}
+                enableStackTooltip={true}
                 legends={[
                     {
-                        anchor: "bottom-right",
+                        anchor: "left",
                         direction: "column",
-                        translateX: 100,
+                        translateX: 20,
                         itemWidth: 80,
                         itemHeight: 20,
                         itemTextColor: "#999999",
@@ -62,33 +76,55 @@ export function ReviewChart({}: {}) {
     );
 }
 
-function createTopicStreamData(articles: Article[]): [StreamDatum[], string[]] {
-    const seenTopics = new Set<string>();
+function createStreamData(annotations: Annotation[]): [StreamDatum[], string[]] {
+    const since = subWeeks(new Date(), 8);
 
-    const weekBuckets: { [week: string]: { [topic: string]: number } } = {};
-    articles.forEach((article) => {
-        const date = new Date(article.time_added * 1000);
-        const year = date.getFullYear();
-        const week = `${year}-${getWeekNumber(date)}`;
+    const seenKeys = new Set<string>();
+    const timeBuckets: { [bucket: string]: { [key: string]: number } } = {};
+    // eachDayOfInterval({
+    //     start: since,
+    //     end: new Date(),
+    // })
+    //     .concat([new Date()])
+    //     .map((date) => {
+    //         const dateStr = date.toISOString().split("T")[0];
+    //         timeBuckets[dateStr] = {};
+    //     });
 
-        if (!weekBuckets[week]) {
-            weekBuckets[week] = {};
-        }
+    annotations
+        .filter((a) => a.created_at * 1000 >= since.getTime())
+        .sort((a, b) => a.created_at - b.created_at)
+        .forEach((annotation) => {
+            const date = new Date(annotation.created_at * 1000);
+            const day = date.toISOString().split("T")[0];
+            const week = `${date.getFullYear()}-${getWeekNumber(date)}`;
 
-        const key = article.publication_date?.slice(0, 4);
-        if (key) {
-            weekBuckets[week][key] = (weekBuckets[week][key] || 0) + 1;
-            seenTopics.add(key);
-        }
-    });
+            const bucket = week;
+            if (!timeBuckets[bucket]) {
+                timeBuckets[bucket] = {};
+            }
 
-    Object.keys(weekBuckets).forEach((week) => {
-        seenTopics.forEach((topic) => {
-            if (!weekBuckets[week][topic]) {
-                weekBuckets[week][topic] = 0;
+            // annotation.tags?.map((tag) => {
+            //     seenKeys.add(tag);
+            //     timeBuckets[bucket][tag] = (timeBuckets[bucket][tag] || 0) + 1;
+            // });
+            // seenKeys.add("total");
+            // timeBuckets[bucket]["total"] = (timeBuckets[bucket]["total"] || 0) + 1;
+
+            const randomTags = ["#reading", "#note-taking", "#writing"];
+            const tag = randomTags[Math.floor(Math.random() * randomTags.length)];
+            seenKeys.add(tag);
+            timeBuckets[bucket][tag] = (timeBuckets[bucket][tag] || 0) + 1;
+        });
+
+    // fill in all key counts for every time bucket
+    Object.keys(timeBuckets).forEach((bucket) => {
+        seenKeys.forEach((topic) => {
+            if (!timeBuckets[bucket][topic]) {
+                timeBuckets[bucket][topic] = 0;
             }
         });
     });
 
-    return [Object.values(weekBuckets).reverse(), Array.from(seenTopics)];
+    return [Object.values(timeBuckets), Array.from(seenKeys)];
 }
