04891b728a38fb471420c06963d90f68e93cf5f7
Peter Hagen <pha@hey.com>
2023-01-16 21:04:37 +0100

    Make article text fetch more resilient

--- apps/serverless-import/src/fetch.ts
+++ apps/serverless-import/src/fetch.ts
@@ -25,11 +25,34 @@ export async function fetchArticleParagraphs(url: string): Promise<string[] | un
         return;
     }
 
+    let paragraphs: string[] = [];
+
+    // try readability
     const reader = new Readability(document);
     const article = reader.parse();
+    const readabilityText = article?.textContent;
 
-    // split paragraphs
-    let paragraphs = article?.textContent?.split("\n\n") || [];
+    // TODO this doesn't seem to work
+    const innerText = naiveInnerText(document.body);
+    console.log(readabilityText?.length, innerText.length);
+
+    if (readabilityText && readabilityText.length >= 0.5 * innerText.length) {
+        // assume readability is working if includes min. 50% of the text
+
+        // split paragraphs for better sentence combine, but it's not that important
+        // filtering by single \n breaks http://paulgraham.com/own.html
+        paragraphs = article.textContent.split("\n\n");
+    } else {
+        // otherwise use all body text
+        // one paragraph because non-visible newlines might occur within sentences
+        // e.g. needed for http://paulgraham.com/read.html
+        paragraphs = [innerText];
+    }
+
+    // clean
+    paragraphs = paragraphs
+        .map((p) => p.replace(/\n/g, " ").replace(/\s+/g, " ").trim())
+        .filter((p) => p.length >= 100);
 
     console.log(
         `Fetched ${paragraphs.length} article paragraphs in ${Math.round(
@@ -38,4 +61,19 @@ export async function fetchArticleParagraphs(url: string): Promise<string[] | un
     );
 
     return paragraphs;
+}
+
+function naiveInnerText(node: Node) {
+    const Node = node; // We need Node(DOM's Node) for the constants, but Node doesn't exist in the nodejs global space, and any Node instance references the constants through the prototype chain
+    return [...node.childNodes]
+        .map((node) => {
+            if (node.nodeType === Node.TEXT_NODE) {
+                return node.textContent;
+            }
+            if (node.childNodes) {
+                return naiveInnerText(node);
+            }
+            return "";
+        })
+        .join("\n");
 }
