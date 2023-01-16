df2097e06888c8b4c9a91cb470ad2b1539d422ce
Peter Hagen <pha@hey.com>
2023-01-16 21:45:20 +0100

    Add import function cors

--- apps/serverless-import/src/index.ts
+++ apps/serverless-import/src/index.ts
@@ -6,6 +6,10 @@ import "@tensorflow/tfjs-node";
 loadHeatmapModel();
 
 export async function main(req: Request, res: Response) {
+    if (handledCors(req, res)) {
+        return;
+    }
+
     const url: string = req.body?.url;
     let paragraphs: string[] | undefined = req.body?.paragraphs;
     if (url) {
@@ -20,4 +24,17 @@ export async function main(req: Request, res: Response) {
     const sentences = await getHeatmap(paragraphs, 300, 100);
 
     res.send({ paragraphs, sentences });
+}
+
+export function handledCors(req: Request, res: Response<any>): boolean {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    if (req.method === "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+
+        res.status(204).json({});
+        return true;
+    }
+
+    return false;
 }
