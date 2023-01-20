fee6aaff22e735f962e81a035e7444bde33ec303
Peter Hagen <pha@hey.com>
2023-01-20 10:02:26 +0100

    Remove related fetch from highlights.ts

--- apps/unclutter/source/content-script/modifications/DOM/smartHighlights.ts
+++ apps/unclutter/source/content-script/modifications/DOM/smartHighlights.ts
@@ -1,17 +1,10 @@
 import { reportEventContentScript } from "@unclutter/library-components/dist/common/messaging";
 import browser from "../../../common/polyfill";
 import { PageModifier, trackModifierExecution } from "../_interface";
-import { sendIframeEvent } from "../../../common/reactIframe";
 import { getUrlHash } from "@unclutter/library-components/dist/common/url";
-import {
-    fetchRelatedAnnotations,
-    indexAnnotationVectors,
-    RelatedHighlight,
-} from "@unclutter/library-components/dist/common/api";
-import { ReplicacheProxy } from "@unclutter/library-components/dist/common/replicache";
 import type { Annotation } from "@unclutter/library-components/dist/store/_schema";
 import { createAnnotations, listParagraphs } from "@unclutter/heatmap/dist/anchor";
-import { RankedSentence } from "@unclutter/heatmap/dist/heatmap";
+import type { RankedSentence } from "@unclutter/heatmap/dist/heatmap";
 
 // analyse an article page and highlight key sentences using an in-browser AI model
 @trackModifierExecution
@@ -29,40 +22,6 @@ export default class SmartHighlightsModifier implements PageModifier {
     constructor(user_id: string) {
         this.user_id = user_id;
         this.article_id = getUrlHash(window.location.href);
-
-        window.addEventListener("message", (event) => this.handleMessage(event.data || {}));
-    }
-
-    private generatedAnnotations: boolean = false;
-    async fetchAnnotations(fetchSaved = true): Promise<boolean> {
-        if (fetchSaved) {
-            // fetch existing user annotations locally
-            const rep = new ReplicacheProxy();
-            this.annotations = await rep.query.listArticleAnnotations(this.article_id);
-            console.log(`Found ${this.annotations.length} local annotations for article`);
-        } else {
-            this.annotations = [];
-        }
-
-        // if no ai annotations saved, create them
-        const aiAnnotations = this.annotations.filter((a) => a.ai_created);
-        if (aiAnnotations.length === 0) {
-            const newAnnotations = await this.parseAnnotationsFromArticle();
-            this.annotations.push(...newAnnotations);
-            this.generatedAnnotations = true;
-        }
-
-        // likely not an article if no annotations present or generated
-        this.annotationsCount = this.annotations.length;
-        if (this.annotations.length === 0) {
-            return false;
-        }
-
-        if (this.saveOnGenerated) {
-            this.saveAnnotations();
-        }
-
-        return true;
     }
 
     async parseAnnotationsFromArticle(): Promise<Annotation[]> {
@@ -109,80 +68,5 @@ export default class SmartHighlightsModifier implements PageModifier {
         });
 
         return newAnnotations;
-    }
-
-    private relatedPerAnnotation: RelatedHighlight[][];
-    async fetchRelated(): Promise<void> {
-        if (!this.annotations || this.annotations.length === 0) {
-            return;
-        }
-
-        // fetch user highlights that are related to the found article annotations
-        const relatedPerAnnotation = await fetchRelatedAnnotations(
-            this.user_id,
-            this.article_id,
-            this.annotations.map((a) => a.quote_text),
-            this.relatedThreshold,
-            false
-        );
-
-        this.relatedCount = 0;
-        relatedPerAnnotation.forEach((related) => {
-            this.relatedCount += related.length;
-        });
-
-        // send to sidebar if already ready
-        // this.relatedPerAnnotation = relatedPerAnnotation;
-        // this.sendAnnotationsToSidebar();
-    }
-
-    private handleMessage(message: any) {
-        if (message.type === "sendAIAnnotationsToSidebar") {
-            // event sent from AnnotationsModifier once sidebar is ready
-            // reply with annotations (only if related done)
-            // if (this.relatedPerAnnotation) {
-            //     this.sendAnnotationsToSidebar();
-            // }
-        }
-    }
-
-    private sendAnnotationsToSidebar() {
-        const sidebarIframe = document.getElementById("lindy-annotations-bar") as HTMLIFrameElement;
-        if (sidebarIframe && this.annotations.length > 0) {
-            sendIframeEvent(sidebarIframe, {
-                event: "setAIAnnotations",
-                annotations: this.annotations,
-                relatedPerAnnotation: this.relatedPerAnnotation,
-            });
-        }
-    }
-
-    private saveOnGenerated = false;
-    async saveAnnotations() {
-        if (!this.generatedAnnotations) {
-            this.saveOnGenerated = true;
-            return;
-        }
-
-        const aiAnnotations = this.annotations?.filter((a) => a.ai_created);
-        if (!aiAnnotations || aiAnnotations.length === 0) {
-            return;
-        }
-        console.log(`Saving ${aiAnnotations.length} AI highlights...`);
-
-        // save locally
-        const rep = new ReplicacheProxy();
-        await Promise.all(
-            aiAnnotations.map(async (annotation) => rep.mutate.putAnnotation(annotation))
-        );
-
-        // save embeddings
-        await indexAnnotationVectors(
-            this.user_id,
-            this.article_id,
-            aiAnnotations.map((a) => a.quote_text),
-            aiAnnotations.map((a) => a.id),
-            false
-        );
     }
 }
