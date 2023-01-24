c3ba570fb57cbaa4fb5ff1f6e90aec31a550e1bb
Peter Hagen <pha@hey.com>
2023-01-24 16:01:14 +0100

    Fix bookmarks import schema

--- apps/library-web/src/new/Import/Bookmarks.tsx
+++ apps/library-web/src/new/Import/Bookmarks.tsx
@@ -1,3 +1,4 @@
+import type { BookmarkedPage } from "@unclutter/library-components/dist/common";
 import { useEffect } from "react";
 
 export function BookmarksImportText({}) {
@@ -8,7 +9,14 @@ export function BookmarksImportButtons({ onError, startImport }) {
     useEffect(() => {
         const listener = (event: MessageEvent) => {
             if (event.data.event === "returnBrowserBookmarks") {
-                startImport(event.data.bookmarks);
+                const bookmarks: BookmarkedPage[] = event.data.bookmarks;
+                const importData = {
+                    urls: bookmarks.map(({ url }) => url),
+                    time_added: bookmarks.map(({ time_added }) => time_added),
+                    favorite: bookmarks.map(({ favorite }) => favorite),
+                };
+
+                startImport(importData);
             }
         };
         window.addEventListener("message", listener);
