65e9028c78e305fa89133b237c9082e723ef52b6
Peter Hagen <pha@hey.com>
2023-02-19 15:50:43 +0100

    Prompt extension update on about page

--- common/library-components/src/components/Modal/About.tsx
+++ common/library-components/src/components/Modal/About.tsx
@@ -3,7 +3,7 @@ import { ModalStateContext } from "./context";
 import { SettingsButton, SettingsGroup } from "../Settings/SettingsGroup";
 import { ReplicacheContext } from "../../store";
 import { useUser } from "@supabase/auth-helpers-react";
-import { setUnclutterLibraryAuth } from "../../common";
+import { getUnclutterVersion, setUnclutterLibraryAuth } from "../../common";
 import { usePaymentsLink } from "../Settings/SmartReading";
 import clsx from "clsx";
 import { getActivityColor } from "../Charts";
@@ -43,6 +43,11 @@ export default function AboutModalTab({}: {}) {
     }, [rep, userInfo]);
 
     const paymentsLink = usePaymentsLink(userInfo);
+
+    const [unclutterVersion, setUnclutterVersion] = useState<string>();
+    useEffect(() => {
+        getUnclutterVersion().then(setUnclutterVersion);
+    }, []);
 
     return (
         <div className="animate-fadein flex flex-col gap-4">
@@ -101,6 +106,35 @@ export default function AboutModalTab({}: {}) {
                     </>
                 )}
             </SettingsGroup>
+
+            {userInfo?.aiEnabled && unclutterVersion && unclutterVersion < "1.7.0" && (
+                <SettingsGroup
+                    title="Update Unclutter"
+                    icon={
+                        <svg className="h-4 w-4" viewBox="0 0 512 512">
+                            <path
+                                fill="currentColor"
+                                d="M506.3 417l-213.3-364C284.8 39 270.4 32 256 32C241.6 32 227.2 39 218.1 53l-213.2 364C-10.59 444.9 9.851 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM52.58 432L255.1 84.8L459.4 432H52.58zM256 337.1c-17.36 0-31.44 14.08-31.44 31.44c0 17.36 14.11 31.44 31.48 31.44s31.4-14.08 31.4-31.44C287.4 351.2 273.4 337.1 256 337.1zM232 184v96C232 293.3 242.8 304 256 304s24-10.75 24-24v-96C280 170.8 269.3 160 256 160S232 170.8 232 184z"
+                            />
+                        </svg>
+                    }
+                    buttons={
+                        <SettingsButton
+                            title="Update extension"
+                            href="https://github.com/lindylearn/unclutter/blob/main/docs/updating.md"
+                            darkModeEnabled={darkModeEnabled}
+                            reportEvent={reportEvent}
+                        />
+                    }
+                    style={{ background: getActivityColor(1, darkModeEnabled) }}
+                >
+                    <p>
+                        Some of these features require the newest version of the Unclutter
+                        extension. Please manually update Unclutter from {unclutterVersion} to
+                        1.7.0.
+                    </p>
+                </SettingsGroup>
+            )}
 
             <SettingsGroup
                 title="Save articles"
