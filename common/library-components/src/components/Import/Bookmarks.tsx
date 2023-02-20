e5b45a9193ef036736e1400b852cc21a50e6b9cd
Peter Hagen <pha@hey.com>
2023-02-20 13:35:18 +0100

    Remove unused import code

--- common/library-components/src/components/Import/Bookmarks.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BrowserBookmarksImportSettings({}) {
-    return (
-        <div className="p-3 text-center">
-            Click the Unclutter extension icon to import your article bookmarks.
-        </div>
-    );
-}
