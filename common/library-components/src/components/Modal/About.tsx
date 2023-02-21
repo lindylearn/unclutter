503ec0d6719431fb8e6251b3387e905c56d5afb1
Peter Hagen <pha@hey.com>
2023-02-21 13:45:28 +0100

    Reorder about messages

--- common/library-components/src/components/Modal/About.tsx
+++ common/library-components/src/components/Modal/About.tsx
@@ -2,8 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ModalStateContext } from "./context";
 import { SettingsButton, SettingsGroup } from "../Settings/SettingsGroup";
 import { ReplicacheContext } from "../../store";
-import { useUser } from "@supabase/auth-helpers-react";
-import { getBrowserTypeWeb, getUnclutterVersion, setUnclutterLibraryAuth } from "../../common";
+import { getBrowserTypeWeb, getUnclutterVersion } from "../../common";
 import { usePaymentsLink } from "../Settings/SmartReading";
 import clsx from "clsx";
 import { getActivityColor } from "../Charts";
@@ -235,26 +234,7 @@ export default function AboutModalTab({}: {}) {
             )}
 
             <SettingsGroup
-                title="Save articles"
-                icon={
-                    <svg className="h-4" viewBox="0 0 576 512">
-                        <path
-                            fill="currentColor"
-                            d="M540.9 56.77C493.8 39.74 449.6 31.58 410.9 32.02C352.2 32.96 308.3 50 288 59.74C267.7 50 223.9 32.98 165.2 32.04C125.8 31.35 82.18 39.72 35.1 56.77C14.02 64.41 0 84.67 0 107.2v292.1c0 16.61 7.594 31.95 20.84 42.08c13.73 10.53 31.34 13.91 48.2 9.344c118.1-32 202 22.92 205.5 25.2C278.6 478.6 283.3 480 287.1 480s9.37-1.359 13.43-4.078c3.516-2.328 87.59-57.21 205.5-25.25c16.92 4.563 34.5 1.188 48.22-9.344C568.4 431.2 576 415.9 576 399.2V107.2C576 84.67 561.1 64.41 540.9 56.77zM264 416.8c-27.86-11.61-69.84-24.13-121.4-24.13c-26.39 0-55.28 3.281-86.08 11.61C53.19 405.3 50.84 403.9 50 403.2C48 401.7 48 399.8 48 399.2V107.2c0-2.297 1.516-4.531 3.594-5.282c40.95-14.8 79.61-22.36 112.8-21.84C211.3 80.78 246.8 93.75 264 101.5V416.8zM528 399.2c0 .5938 0 2.422-2 3.969c-.8438 .6407-3.141 2.063-6.516 1.109c-90.98-24.6-165.4-5.032-207.5 12.53v-315.3c17.2-7.782 52.69-20.74 99.59-21.47c32.69-.5157 71.88 7.047 112.8 21.84C526.5 102.6 528 104.9 528 107.2V399.2z"
-                        />
-                    </svg>
-                }
-                imageSrc="/media/articles.png"
-            >
-                <p>Every article you open automatically gets saved in your library.</p>
-                <p>
-                    Organize your reading queue with drag & drop, read on your mobile phone, or find
-                    anything you've seen in the past.
-                </p>
-            </SettingsGroup>
-
-            <SettingsGroup
-                title="Read faster"
+                title="Focus on what matters"
                 icon={
                     <svg className="h-4 w-4" viewBox="0 0 576 512">
                         <path
@@ -267,13 +247,12 @@ export default function AboutModalTab({}: {}) {
             >
                 <p>
                     Read faster and focus on what matters by seeing the most important sentences on
-                    each article highlighted in yellow.
+                    each article automatically highlighted in yellow.
                 </p>
-                <p>Click on any highlight to save it in your library.</p>
             </SettingsGroup>
 
             <SettingsGroup
-                title="Collect quotes"
+                title="Save quotes"
                 icon={
                     <svg className="w-4" viewBox="0 0 448 512">
                         <path
@@ -284,10 +263,12 @@ export default function AboutModalTab({}: {}) {
                 }
                 imageSrc="media/quotes.png"
             >
-                <p>All saved highlights are tagged and organized automatically.</p>
                 <p>
-                    Browse your knowledge base by topic, review your recent notes, or AI search for
-                    everything you've seen about a specific idea.
+                    Click on any highlight to save it in your library, or manually select any text.
+                </p>
+                <p>
+                    All highlights are tagged automatically. Browse your saved quotes by topic,
+                    review your recent notes, or AI search across everything you've read.
                 </p>
             </SettingsGroup>
 
@@ -303,10 +284,29 @@ export default function AboutModalTab({}: {}) {
                 }
                 imageSrc="media/connect_ideas.png"
             >
-                <p>Quotes automatically appear next to related quotes.</p>
+                <p>Saved quotes automatically appear next to related quotes.</p>
                 <p>
                     For example, if a text mentions Google and AI search, you'll see everything
                     you've read about Google's AI research and chatGPT right next to it.
+                </p>
+            </SettingsGroup>
+
+            <SettingsGroup
+                title="Save articles"
+                icon={
+                    <svg className="h-4" viewBox="0 0 576 512">
+                        <path
+                            fill="currentColor"
+                            d="M540.9 56.77C493.8 39.74 449.6 31.58 410.9 32.02C352.2 32.96 308.3 50 288 59.74C267.7 50 223.9 32.98 165.2 32.04C125.8 31.35 82.18 39.72 35.1 56.77C14.02 64.41 0 84.67 0 107.2v292.1c0 16.61 7.594 31.95 20.84 42.08c13.73 10.53 31.34 13.91 48.2 9.344c118.1-32 202 22.92 205.5 25.2C278.6 478.6 283.3 480 287.1 480s9.37-1.359 13.43-4.078c3.516-2.328 87.59-57.21 205.5-25.25c16.92 4.563 34.5 1.188 48.22-9.344C568.4 431.2 576 415.9 576 399.2V107.2C576 84.67 561.1 64.41 540.9 56.77zM264 416.8c-27.86-11.61-69.84-24.13-121.4-24.13c-26.39 0-55.28 3.281-86.08 11.61C53.19 405.3 50.84 403.9 50 403.2C48 401.7 48 399.8 48 399.2V107.2c0-2.297 1.516-4.531 3.594-5.282c40.95-14.8 79.61-22.36 112.8-21.84C211.3 80.78 246.8 93.75 264 101.5V416.8zM528 399.2c0 .5938 0 2.422-2 3.969c-.8438 .6407-3.141 2.063-6.516 1.109c-90.98-24.6-165.4-5.032-207.5 12.53v-315.3c17.2-7.782 52.69-20.74 99.59-21.47c32.69-.5157 71.88 7.047 112.8 21.84C526.5 102.6 528 104.9 528 107.2V399.2z"
+                        />
+                    </svg>
+                }
+                imageSrc="/media/articles.png"
+            >
+                <p>Every article you open automatically gets saved in your library.</p>
+                <p>
+                    Organize your reading queue with drag & drop, read on your mobile phone, or find
+                    anything you've seen in the past.
                 </p>
             </SettingsGroup>
 
