a268df7034267b35c23e231da03251825854daf0
Peter Hagen <pha@hey.com>
2023-02-28 15:22:39 +0100

    Fix web posthog domain

--- apps/library-web/common/metrics.ts
+++ apps/library-web/common/metrics.ts
@@ -2,7 +2,8 @@ import posthog from "posthog-js";
 
 export function initPosthog() {
     posthog.init("phc_BQHO9btvNLVEbFC4ihMIS8deK5T6P4d8EF75Ihvkfaw", {
-        api_host: "https://e.lindylearn.io",
+        // api_host: "https://e.lindylearn.io", // needs lambda to rewrite host header, see https://gist.github.com/htnosm/2c51a70f0e361056d1fd63321afba4fa
+        api_host: "https://app.posthog.com",
         loaded: (posthog) => {
             if (process.env.NODE_ENV === "development") {
                 posthog.opt_out_capturing();
